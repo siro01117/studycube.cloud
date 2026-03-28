9e3caf5df6ec637f3a91af6ded8121dfae9a63d9
siro01117 <siro0117@naver.com>
2026-03-28 23:22:19 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './SCP.jsx' // 깃허브에 올린 파일 이름이 SCP.jsx이므로 맞춤
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
